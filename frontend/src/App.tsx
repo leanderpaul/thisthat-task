1daf7033da311c8fd898db0084014bdb6d0db01d
Leander Paul <leanderpaul97@outlook.com>
2023-11-17 14:50:48 +0000

    feat(frontend): home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
 /**
  * Importing npm packages
  */
+import { useState, useEffect } from 'react';
 
 /**
  * Importing user defined components
  */
+import { TextField } from './components/text-field';
 
 /**
  *  Importing user defined modules
  */
+import { useMarvelCharacterList } from '@app/hooks';
+import { CardList } from './components/card-list';
 
 /**
  * Declaring types
@@ -23,10 +27,38 @@
  */
 
 function App() {
+  const [value, setValue] = useState('');
+  const { data, loading, fetch, clear } = useMarvelCharacterList();
+
+  function showAlert(name: string) {
+    alert(`You have selected '${name}'`);
+    setValue('');
+  }
+
+  useEffect(() => {
+    if (value.length > 1) fetch(value);
+    else clear();
+  }, [value]);
+
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <div className="background-image"></div>
+      <div className="container">
+        <div>
+          <span className="title">Marvel</span>
+        </div>
+        <TextField
+          placeholder="Enter marvel character name"
+          value={value}
+          onChange={setValue}
+        />
+        <CardList
+          data={data}
+          loading={loading && data.length === 0}
+          onCardClick={showAlert}
+        />
+      </div>
+    </>
   );
 }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,6 +8,7 @@ import ReactDOM from 'react-dom/client';
  *  Importing user defined modules
  */
 import App from './App.tsx';
+import './main.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
